e7858fd62b187cb5d9c2d59c4ed20500783d09e3
SeppeBr <seppebremmers@gmail.com>
2022-02-16 09:09:28 +0100

    reformat code 2

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,19 +4,19 @@ namespace App\http\Controllers;
 
 class PostsController
 {
-        public function show($post)
+    public function show($post)
     {
-            $posts = [
-                'my-first-post' => 'Hello, this is my first blog post!',
-                'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-            ];
+        $posts = [
+            'my-first-post' => 'Hello, this is my first blog post!',
+            'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+        ];
 
-            if (!array_key_exists($post, $posts)) {
-                abort(404, 'Sorry, that post was not found.');
-            }
-
-            return view('post', [
-                'post' => $posts[$post]
-            ]);
+        if (!array_key_exists($post, $posts)) {
+            abort(404, 'Sorry, that post was not found.');
         }
+
+        return view('post', [
+            'post' => $posts[$post]
+        ]);
     }
+}
